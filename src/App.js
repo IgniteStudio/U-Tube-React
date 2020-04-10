efe82aad1fe5546e21a5ee3c0f219ebb123bec97
David Agaybi <davidagaybi@gmail.com>
2020-04-10 02:10:51 -0400

    add Grids VideoList VideoItem

--- src/App.js
+++ src/App.js
@@ -8,21 +8,32 @@ import youtube from './api/youtube';
 
 
 class App extends React.Component {
+    state = {
+        video: [],
+        selectedVideo: null,
+    }
     handleSubmit = async (searchTerm) => {
-        const response = await youtube.get('search', {params: {q: searchTerm}})
-        console.log(response);
+        const response = await youtube.get('search', { params: {
+            part: 'snippet',
+            maxResults: 5,
+            key: 'AIzaSyB0_3-jZz0jqQr9NJHAgFhoQf_dHjtxZDI',
+            q: searchTerm,
+        }
+    });
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
         
     }
     render(){
+        const {selectedVideo} = this.state;
         return (
-            <Grid justify="center" container spacing={16}>
-                <Grid item xs={12}>
-                    <Grid container spacing={16}>
-                        <Grid item xs={12}>
+            <Grid justify="center" container spacing={10}>
+                <Grid item xs={10}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={10}>
                             <SearchBar onFormSubmit={this.handleSubmit}/>
                         </Grid>
                         <Grid item xs={8}>
-                            <VideoDetail/>
+                            <VideoDetail video={selectedVideo}/>
                         </Grid>
                         <Grid item xs={4}>
                            {/* VIDEO LIST */}
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,9 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: '[AIzaSyB0_3-jZz0jqQr9NJHAgFhoQf_dHjtxZDI]'
-    }
-})
+   
+});
